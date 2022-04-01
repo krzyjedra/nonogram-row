24a6c53c5ef40c45cba1fdd788a2284885be942a
k.jedra <kozukj@gmail.com>
2022-04-01 17:34:30 +0200

    final version

--- src/main/scala/NonogramRow.scala
+++ src/main/scala/NonogramRow.scala
@@ -1,6 +1,8 @@
 object NonogramRow extends App {
 
-  def nonogramrow(ar: Array[Int]): Array[Int] = {
-???
+  def countOneConsecutive(ar: Array[Int]): Array[Int] = {
+    if (ar.head == 0)
+ar.mkString.split("0+").tail.map(_.length)
+    else ar.mkString.split("0+").map(_.length)
   }
 }
--- src/test/scala/NonogramRowSpec.scala
+++ src/test/scala/NonogramRowSpec.scala
@@ -6,7 +6,23 @@ class NonogramRowSpec extends AnyFlatSpec with should.Matchers {
 
   import NonogramRow._
 
-  "nonogramrow" should "be 1 for a" in {
-    ???
+  "methodNon" should "be Array(1, 1) for Array(1,0,1)" in {
+    countOneConsecutive(Array(1,0,1)) shouldBe Array(1,1)
+  }
+
+  it should "be Array(1, 2) for Array(0,1,0,1,1)" in {
+    countOneConsecutive(Array(0,1,0,1,1)) shouldBe Array(1,2)
+  }
+
+  it should "be Array(1, 2) for Array(0,0,0,1,0,1,1)" in {
+    countOneConsecutive(Array(0,0,0,1,0,1,1)) shouldBe Array(1,2)
+  }
+
+  it should "be Array(1, 2) for Array(0,0,0,1,0,0,0,0,1,1,1)" in {
+    countOneConsecutive(Array(0,0,0,1,0,0,0,0,1,1,1)) shouldBe Array(1,3)
+  }
+
+  it should "be Array(1, 2, 4) for Array(0,0,0,1,0,1,1,0,0,1,1,1,1)" in {
+    countOneConsecutive(Array(0,0,0,1,0,1,1,0,0,1,1,1,1)) shouldBe Array(1,2,4)
   }
 }
